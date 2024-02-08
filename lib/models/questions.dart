9bbc9bc2d5f265c17de39594a97161d77092924d
NomuraSan0 <taweesup2727@gmail.com>
2024-02-08 23:54:09 +0700

    git

---
+++ lib/models/questions.dart
@@ -0,0 +1,104 @@
+import 'question.dart';
+
+const List<Question> questions = [
+  Question(
+    question: 'Ferrari รุ่นใดที่เครื่องยนต์วางด้านหน้า?',
+    correctAnswerIndex: 2,
+    options: [
+      'a) Ferrari 360 Modena',
+      'b) Ferrari F355 Berlinetta',
+      'c) Ferrari F12 Berlinetta',
+      'd) Ferrari F430',
+    ],
+  ),
+  Question(
+    question: '2.GT-R ย่อมาจากคำว่าอะไร?',
+    correctAnswerIndex: 2,
+    options: [
+      'a) Gran Turismo Race',
+      'b) Gran Turismo Racing',
+      'c) Gran Turismo Racer',
+      'd) Gran Turismo Raze',
+    ],
+  ),
+  Question(
+    question: '3.รถยี่ห้อใดจัดอยู่ในประเภท Hyper Car?',
+    correctAnswerIndex: 3,
+    options: [
+      'a) Audi',
+      'b) Ferrari',
+      'c) Lamborghini',
+      'd) Bugatti',
+    ],
+  ),
+  Question(
+    question: '4.ชุดแต่งของ Rx-7 ในภาพยนต์เรื่อง Tokyo Drift ซึ่งขับโดย Hahn คือสำนักใด มีชื่อรุ่นบอดี้คิทว่าอะไร',
+    correctAnswerIndex: 0,
+    options: [
+      'a) Veilside-Fortune',
+      'b) Veilside-Fortunez',
+      'c) Veilside-Fortuner',
+      'd) Veilside-Fortunes',
+    ],
+  ),
+  Question(
+    question: '5.Lamborghini murcielago Ip670-4 SV คำว่า " SV " ย่อมาจากอะไร',
+    correctAnswerIndex: 3,
+    options: [
+      'a) Supervelose',
+      'b) Supervoloce',
+      'c) Superveloces',
+      'd) Superveloce',
+    ],
+  ),
+  Question(
+    question: '6.Lamborghini murcielago Ip670-4 SV มีแรงม้าอยู่เท่าใด',
+    correctAnswerIndex: 3,
+    options: [
+      'a) 700 แรงม้า',
+      'b) 690 แรงม้า',
+      'c) 650 แรงม้า',
+      'd) 670 แรงม้า',
+    ],
+  ),
+  Question(
+    question: '7.Ferrari รุ่นใดเป็นรุ่น Light Weight Edition',
+    correctAnswerIndex: 2,
+    options: [
+      'a) Ferrari F430',
+      'b) Ferrari 458 Italia',
+      'c) Ferrari F430 Scuderia',
+      'd) Ferrari F430 Spider',
+    ],
+  ),
+  Question(
+    question: '8.Nissan Skyline GT-R R34 สามารถทำความเร็วจาก 0-100 km ได้ภายในเวลากี่วินาที',
+    correctAnswerIndex: 3,
+    options: [
+      'a) 4.3 วินาที',
+      'b) 4.0 วินาที',
+      'c) 4.7 วินาที',
+      'd) 4.5 วินาที',
+    ],
+  ),
+  Question(
+    question: '9.Audi takeover บริษัทรถ Supercar ยี่ห้อใด',
+    correctAnswerIndex: 3,
+    options: [
+      'a) Ferrari',
+      'b) Mclaren',
+      'c) Porsche',
+      'd) Lamborghini',
+    ],
+  ),
+  Question(
+    question: 'รถรุ่นใดสามารถเนียนไปจอดในโซน Supercar ที่ห้างดังๆได้',
+    correctAnswerIndex: 3,
+    options: [
+      'a) Vios',
+      'b) Vigo',
+      'c) Mr-2',
+      'd) Mr-s',
+    ],
+  )
+];
